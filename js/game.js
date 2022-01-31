4261d455a8923ed3dd35f28b253b3bb39f7f1e9b
admin <imnordi@gmail.com>
2022-01-31 00:07:26 -0300

    scrapping

---
+++ js/game.js
@@ -0,0 +1,20 @@
+//Canvas properties
+const canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+ctx.font = "30px Arial";
+ctx.textAlign = "left";
+ctx.fillText("PALABRA", 0, 200);
+// TODO: get a word from etimologia.dechile.net and keep it--->let palabra = ctx.filletext()
+// use scraping
+//let palabra = ctx.filletext("aWordByScrap,0,200")
+//
+//
+//
+// TODO: get a word from etimologia.dechile.net and keep it--->let opcion = ctx.filletext()
+// use scraping
+//let opcion = ctx.filletext("aWordByScrap,0,200")
+
+ctx.fillText("OPCION", 400, 50);
+ctx.fillText("OPCION", 400, 150);
+ctx.fillText("OPCION", 400, 250);
+ctx.fillText("OPCION", 400, 350);
